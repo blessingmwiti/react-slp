1433ee9fa9c877c344305d4759d0974531bd7a7e
blessingmwiti <blessingmwiti@gmail.com>
2024-01-14 12:29:11 +0300

    add some libraries

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import Navbar from './Navbar';
 
 const Home = () => {
-    return <div>Home</div>;
+    return (<div className='home-container'>
+        <Navbar />
+    </div>);
 };
 
 export default Home;
